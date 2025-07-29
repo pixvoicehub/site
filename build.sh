6e17bc61e79e5f69e09749a7a87ca73394da136a
pixvoicehub <pix.voicehub@gmail.com>
2025-07-29 13:22:42 -0300

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# Instala o pip mais recente
-pip install --upgrade pip
-
-# Instala as dependências do requirements.txt
-pip install -r requirements.txt
